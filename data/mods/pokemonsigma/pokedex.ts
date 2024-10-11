b12d3e3ebdff213aa0d6d4d4e18372795faa9243
Bobby-is-the-worst <129467965+Bobby-is-the-worst@users.noreply.github.com>
2024-10-11 12:39:49 +1100

    Update pokedex.ts

--- data/mods/pokemonsigma/pokedex.ts
+++ data/mods/pokemonsigma/pokedex.ts
@@ -1,3 +1,15 @@
 export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
-
+	thermakor : {
+		num: 1026,
+		name: "Thermakor",
+		types: ["Rock", "Fire"],
+		gender: "N",
+		baseStats: {hp: 130, atk: 85, def: 75, spa: 150, spd: 160, spe: 70},
+		abilities: {0: "Volcanic Ash"},
+		heightm: 30,
+		weightkg: 999,
+		color: "Brown",
+		tags: ["Restricted Legendary"],
+		eggGroups: ["Undiscovered"],
+	}
 };
